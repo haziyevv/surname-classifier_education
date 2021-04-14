0c680fdaf28666538025a419336334f68a7d1138
haziyevv <farid@buglance.com>
2021-04-14 15:22:55 +0400

    Applied my own way without looking at the book

--- surname_classification/models/surname_classifier.py
+++ surname_classification/models/surname_classifier.py
@@ -3,28 +3,18 @@ import torch.nn.functional as F
 
 
 class SurnameClassifier(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim):
-        """
-        Args:
-            input_dim:
-            hidden_dim:
-            output_dim:
-        """
+    def __init__(self, input_dim, hidden_dim, out_dim):
         super(SurnameClassifier, self).__init__()
         self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        self.fc2 = nn.Linear(hidden_dim, out_dim)
 
-    def forward(self, x_in, apply_softmax=False):
+    def forward(self, x):
         """
         Args:
-            x_in:
-            apply_softmax:
+            x:
         Returns:
         """
-        intermediate_vector = F.relu(self.fc1(x_in))
-        prediction_vector = self.fc2(intermediate_vector)
+        out = F.relu(self.fc1(x))
+        out = self.fc2(out)
 
-        if apply_softmax:
-            prediction_vector = F.softmax(prediction_vector,dim=1)
-
-        return prediction_vector
+        return out
